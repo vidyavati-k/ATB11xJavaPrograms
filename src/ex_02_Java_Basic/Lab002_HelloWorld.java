e68bd9b1f73d674772849fe8d02350374c3606b3
vidya <vidyavati10@gmail.com>
2025-04-14 19:42:40 +0530

    commit

---
+++ src/ex_02_Java_Basic/Lab002_HelloWorld.java
@@ -0,0 +1,19 @@
+package ex_02_Java_Basic;
+
+public class Lab002_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        int a =10;
+        String name="vidya";
+        float number=10.6f;
+        System.out.println(name);
+
+        for(int i =0;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+
+
+    }
+
+}
